0d11b8df49222c1dcf6c9c9391aab388b4c1a6e5
unnamed42 <wenruih.cn@gmail.com>
2019-08-21 16:04:06 +0800

    resolve test no runnable method

--- src/test/java/com/tjh/riskfactor/RiskFactorApplicationTests.java
+++ src/test/java/com/tjh/riskfactor/RiskFactorApplicationTests.java
@@ -1,11 +1,5 @@
 package com.tjh.riskfactor;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class RiskFactorApplicationTests {
 
 }
