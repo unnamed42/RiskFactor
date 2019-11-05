746323b6b3fcc923066db25d06e9062e6ecb9056
unnamed42 <wenruih.cn@gmail.com>
2019-11-06 00:34:31 +0800

    copy-webpack-plugin

--- webpack.pro.js
+++ webpack.pro.js
@@ -2,9 +2,13 @@ const merge = require("webpack-merge");
 const common = require("./scripts/webpack");
 // const WebpackCdnPlugin = require("webpack-cdn-plugin");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
   plugins: [
+    new CopyPlugin([
+      { from: "public", to: "public" }
+    ]),
     // new WebpackCdnPlugin({
     //   modules: [{
     //     name: "react-dom",
