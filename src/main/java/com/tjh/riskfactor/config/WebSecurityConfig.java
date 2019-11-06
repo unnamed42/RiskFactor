773d303adc31597e1de8d9c57f5247dee28b2b85
unnamed42 <wenruih.cn@gmail.com>
2019-11-06 11:47:10 +0800

    cleanup

--- src/main/java/com/tjh/riskfactor/config/WebSecurityConfig.java
+++ src/main/java/com/tjh/riskfactor/config/WebSecurityConfig.java
@@ -4,13 +4,8 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -27,9 +22,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import com.tjh.riskfactor.error.ErrorResponder;
 import com.tjh.riskfactor.security.JwtTokenFilter;
 import com.tjh.riskfactor.security.JwtUserDetailsService;
-
-import java.util.Arrays;
-import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -60,24 +52,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         provider.setHideUserNotFoundExceptions(false);
         return provider;
     }
-
-//    /**
-//     * 开启跨域请求支持，开放给本机，目前只能作为debug使用
-//     * TODO: 开放给前端实际IP，用于生产环境
-//     */
-//    @Bean
-//    protected FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
-//        var source = new UrlBasedCorsConfigurationSource();
-//        var config = new CorsConfiguration();
-//        config.setAllowCredentials(true);
-//        config.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://127.0.0.1:8080"));
-//        config.setAllowedMethods(Collections.singletonList("*"));
-//        config.setAllowedHeaders(Collections.singletonList("*"));
-//        source.registerCorsConfiguration("/**", config);
-//        var bean = new FilterRegistrationBean<>(new CorsFilter(source));
-//        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-//        return bean;
-//    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder builder) throws Exception {
