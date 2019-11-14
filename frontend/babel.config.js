2b29752822c76a067615883964dcfcffac0d4d69
unnamed42 <wenruih.cn@gmail.com>
2019-11-14 14:32:34 +0800

    babel-plugin-proposal-class-decorator

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -8,7 +8,8 @@ module.exports = {
         "@babel/plugin-proposal-optional-chaining",
         "@babel/plugin-proposal-nullish-coalescing-operator",
         "@babel/plugin-syntax-dynamic-import",
-        "@babel/proposal-class-properties",
+        ["@babel/plugin-proposal-decorators", { legacy: true }],
+        ["@babel/plugin-proposal-class-properties", { loose: true }],
         "@babel/proposal-object-rest-spread",
         "babel-plugin-transform-async-to-promises",
         // enables this to use more modern features
