9d631c6142fda2e42ebc47eadc565a7f70054621
unnamed42 <wenruih.cn@gmail.com>
2019-11-27 13:34:18 +0800

    excel uploading WIP - 2

--- frontend/src/utils/excel.ts
+++ frontend/src/utils/excel.ts
@@ -1,8 +1,8 @@
 import { CellRichTextValue, ValueType, Workbook, Worksheet } from "exceljs";
 
-import { assign, isNil } from "lodash";
+import { isNil, set } from "lodash";
 
-import { cachedLayout } from "@/utils";
+import { cachedLayout, tuple } from "@/utils";
 import { Dict, Question } from "@/types";
 
 const header = (h1: string, h2: string | undefined) =>
@@ -11,34 +11,39 @@ const header = (h1: string, h2: string | undefined) =>
 const slashJoin = (...args: Array<string | undefined | null>) =>
   args.filter(arg => !isNil(arg)).join("/");
 
+interface QInfo extends Question {
+  parent?: QInfo;
+}
+
 const titleMapping = (layout: Dict<Question[]>) => {
-  const result: Dict<Question> = {};
-  const addList = (title: string, list: Question[]) => {
-    list.forEach(q => {
-      result[slashJoin(title, q.label)] = q;
-    });
+  const result: Dict<QInfo> = {};
+  const collect = (title: string, root: Question, parent?: QInfo) => {
+    if(root.type === undefined || root.label === undefined)
+      return;
+    const curr = parent ? { ...root, parent } : root;
+    const currTitle = slashJoin(title, root.label);
+    result[currTitle] = curr;
+    if(root.type === "table") {
+      const [, ...rows] = root.list!;
+      rows.map(r => r.list!).forEach(row => {
+        collect(slashJoin(currTitle, row[0].label), row[1], curr);
+      });
+    } else
+      root.list?.forEach(q => collect(slashJoin(title, root.label), q, curr));
   };
-  Object.entries(layout).forEach(([title, list]) => {
-    list?.forEach(q => {
-      switch (q.type) {
-        case "template":
-        case "table":
-          addList(title, q.list!);
-          break;
-        case undefined:
-          return;
-        default:
-          if (!q.label) {
-            console.error(`level 1 question ${q.id} has no label`);
-            return;
-          }
-          result[slashJoin(title, q.label)] = q;
-      }
-      if (q.enabler)
-        addList(title, q.list!);
-    });
-  });
+  Object.entries(layout).forEach(([title, list]) => list?.forEach(q => collect(title, q)));
   return result;
+};
+
+const propsOf = (info: QInfo, value: string) => {
+  const path = [];
+  for(let p: QInfo | undefined = info; p !== undefined; ) {
+    path.push(p.id);
+    p = p.parent;
+  }
+  if(info?.parent?.enabler)
+    path.push("#enabler");
+  return [tuple(path.reverse().map(id => `$${id}`).join("."), value)];
 };
 
 const valueOf = (value: CellRichTextValue) =>
@@ -48,6 +53,8 @@ const parseCells = (sheet: Worksheet, layout: Dict<Question[]>) => {
   const mapping = titleMapping(layout);
   const { rowCount, columnCount } = sheet;
   const result = [];
+
+  console.log(mapping);
 
   for (let r = 4; r <= rowCount; ++r) {
     const entry = {};
@@ -67,13 +74,10 @@ const parseCells = (sheet: Worksheet, layout: Dict<Question[]>) => {
         console.error(`问卷样式错误：不存在内容 ${header(h1, h2)}`);
         continue;
       }
-      console.log(value);
       let strValue = value.toString();
       if (type === ValueType.RichText)
         strValue = valueOf(value as CellRichTextValue);
-      assign(entry, {
-        [`$${qu.id}`]: strValue
-      });
+      propsOf(qu, strValue).forEach(([k, v]) => set(entry, k, v));
     }
     result.push(entry);
   }
